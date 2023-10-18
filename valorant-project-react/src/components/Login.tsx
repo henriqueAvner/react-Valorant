713ffc5e3ff3b2eb7efbd3f192f5fa95295ee007
henriqueAvner <avnerhdpb@gmail.com>
2023-10-18 17:06:13 -0300

    Feat: Adicionando estilização a pagina inicial

--- valorant-project-react/src/components/Login.tsx
+++ valorant-project-react/src/components/Login.tsx
@@ -2,26 +2,44 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { userAccess } from './redux/actions/actions';
+import styles from './styles/Login.module.css';
 
 export default function Login() {
   const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
   const dispatch = useDispatch();
   function HandleClick() {
     dispatch(userAccess(login));
     navigate('/home');
   }
+  function HandleLogin(event: any) {
+    setLogin(event.target.value);
+  }
+  function HandlePassword(e: any) {
+    setPassword(e.target.value);
+  }
 
   return (
-    <form>
-      <h3>Digite seu login:</h3>
+    <form className={ styles.formClass }>
+      <h3>LOG IN:</h3>
       <input
+        className={ styles.input1 }
         type="text"
+        placeholder="username"
         value={ login }
-        onChange={ (event) => setLogin(event.target.value) }
+        onChange={ HandleLogin }
+      />
+      <input
+        className={ styles.input2 }
+        type="password"
+        value={ password }
+        placeholder="password"
+        onChange={ HandlePassword }
       />
       <div>
         <button
+          className={ styles.ClassButton }
           disabled={ login.length < 3 }
           type="button"
           onClick={ HandleClick }
